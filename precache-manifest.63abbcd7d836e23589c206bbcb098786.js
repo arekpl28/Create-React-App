8251f626dba95b97332c4a1d2c50be8745d37cfc
Arkadiusz Plumbaum <arekpl28@wp.pl>
2019-05-13 14:45:51 +0200

    Updates

---
+++ precache-manifest.63abbcd7d836e23589c206bbcb098786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b311f775661ddcce199b9832b4bf67b1",
+    "url": "/Create-React-App/index.html"
+  },
+  {
+    "revision": "67e8ca936dfb47ae6c0a",
+    "url": "/Create-React-App/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Create-React-App/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "67e8ca936dfb47ae6c0a",
+    "url": "/Create-React-App/static/js/main.32b94c96.chunk.js"
+  },
+  {
+    "revision": "0cbb59032d032c485b36",
+    "url": "/Create-React-App/static/js/runtime~main.14d338cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Create-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
